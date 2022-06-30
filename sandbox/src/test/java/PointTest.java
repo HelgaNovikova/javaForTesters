c577639fa9ec29a0914b61eaed73d5055ab72723
HelgaNovikova <helpotaki@rambler.ru>
2022-06-30 11:36:11 -0400

    divide into separate tests

--- sandbox/src/test/java/PointTest.java
+++ sandbox/src/test/java/PointTest.java
@@ -4,13 +4,24 @@ import org.testng.annotations.Test;
 public class PointTest {
 
     @Test
-    public void testDistance(){
-        Point point1 = new Point(0, 0);
-        Point point2 = new Point(0, 1);
-        Assert.assertEquals(point1.distance(point2), 1);
-        Assert.assertEquals(point1.distance(point1), 0);
-        Point point3 = new Point(2.5, -7);
-        Assert.assertEquals(point2.distance(point3), 8.381527307120106);
+    public void positiveCoordinates(){
+        Point point1 = new Point(10, 19.3);
+        Point point2 = new Point(2.5, 7);
+        Assert.assertEquals(point1.distance(point2), 14.406248644251564);
+    }
 
+    @Test
+    public void negativeCoordinates(){
+        Point point1 = new Point(-10, -19.3);
+        Point point2 = new Point(-2.5, -7);
+        Assert.assertEquals(point1.distance(point2), 14.406248644251564);
+    }
+
+    @Test
+    public void theSameCoordinates(){
+        Point point1 = new Point(9, -4.4);
+        Point point2 = new Point(9, -4.4);
+        Assert.assertEquals(point1.distance(point2), 0);
+        Assert.assertEquals(point1.distance(point1), 0);
     }
 }
