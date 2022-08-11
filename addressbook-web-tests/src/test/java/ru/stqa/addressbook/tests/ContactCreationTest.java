31ce1e2a129e2634cf80589a438353f64a303f1a
HelgaNovikova <helpotaki@rambler.ru>
2022-08-11 17:23:06 -0400

    added reading data from file

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/ContactCreationTest.java
@@ -1,25 +1,50 @@
 package ru.stqa.addressbook.tests;
 
+import com.google.gson.Gson;
+import org.openqa.selenium.json.TypeToken;
 import org.testng.annotations.*;
 import ru.stqa.addressbook.model.ContactData;
 import ru.stqa.addressbook.model.Contacts;
+import ru.stqa.addressbook.model.GroupData;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTest extends TestBase {
 
-  @Test
-  public void testContactCreation() throws Exception {
-    app.getNavigationHelper().goHome();
-    Contacts contactsBefore = app.getContactHelper().getContactsSet();
-    app.getNavigationHelper().gotoAddNewContact();
-    ContactData newContact = new ContactData().withFirstName("Olga").withLastName("Novikova").withEmail("dfgdffg@sf.ru").withHomePhoneNumber("567789789");
-    app.getContactHelper().createContact(newContact);
-    app.getNavigationHelper().goHome();
-    Contacts contactsAfter = app.getContactHelper().getContactsSet();
-    newContact.withId(contactsAfter.stream().mapToInt((g) -> g.getId()).max().getAsInt());
-    assertThat(contactsAfter, equalTo(contactsBefore.withAdded(newContact)));
-  }
+    @DataProvider
+    public Iterator <Object[]> validContactsFromJson() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+        String json = "";
+        String line = reader.readLine();
+        while (line != null){
+            json = json + line;
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List <ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());
+        return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+    }
+
+    @Test(dataProvider = "validContactsFromJson")
+    public void testContactCreation(ContactData newContact) throws Exception {
+        app.getNavigationHelper().goHome();
+        Contacts contactsBefore = app.getContactHelper().getContactsSet();
+        app.getNavigationHelper().gotoAddNewContact();
+        app.getContactHelper().createContact(newContact);
+        app.getNavigationHelper().goHome();
+        Contacts contactsAfter = app.getContactHelper().getContactsSet();
+        newContact.withId(contactsAfter.stream().mapToInt((g) -> g.getId()).max().getAsInt());
+        assertThat(contactsAfter, equalTo(contactsBefore.withAdded(newContact)));
+    }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/GroupCreationTests.java
@@ -1,18 +1,53 @@
 package ru.stqa.addressbook.tests;
 
+import com.google.gson.Gson;
+import org.openqa.selenium.json.TypeToken;
 import org.testng.annotations.*;
 import ru.stqa.addressbook.model.GroupData;
 import ru.stqa.addressbook.model.Groups;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() throws Exception {
+  @DataProvider
+  public Iterator <Object[]> validGroups() throws IOException {
+    List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+    String line = reader.readLine();
+    while (line != null){
+      String[] split = line.split(";");
+      list.add(new Object[]{new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+      line = reader.readLine();
+    }
+    return list.iterator();
+  }
+
+  @DataProvider
+  public Iterator <Object[]> validGroupsFromJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.json")));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null){
+      json = json + line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List <GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>(){}.getType());
+    return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+  }
+
+  @Test(dataProvider = "validGroupsFromJson")
+  public void testGroupCreation(GroupData newGroup){
     app.getNavigationHelper().gotoGroups();
     Groups groupsBefore = app.getGroupHelper().getGroupSet();
-    GroupData newGroup = new GroupData().withName("novikova1").withHeader("novikovaHeader").withFooter("novikovaFooter");
     app.getGroupHelper().createGroup(newGroup);
     app.getNavigationHelper().gotoGroups();
     Groups groupsAfter = app.getGroupHelper().getGroupSet();
